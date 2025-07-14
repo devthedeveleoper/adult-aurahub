e6f68964faa1488539716fd679bea65174417560
devthedeveleoper <ashwathama249@gmail.com>
2025-07-14 14:56:44 +0530

    Init

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['thumb.tapecontent.net', 'amazing-axolotl-e79527.netlify.app'],
-  },
-};
-
-export default nextConfig;
